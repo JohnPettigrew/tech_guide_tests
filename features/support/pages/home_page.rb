58bbc8c3dc0852dfa42298848b2052cb2f7f0cbf
John Pettigrew <jpettigrew@parkinsons.org.uk>
2024-08-30 13:38:15 +0100

    Refactor home_page object for better structure

--- features/support/pages/home_page.rb
+++ features/support/pages/home_page.rb
@@ -5,17 +5,21 @@ module Pages
 
   class HomePage < TestEvolve::Core::PageObject
     attr_accessor :interesting_product_name, :interesting_product_url
-    element(:hero_title) { h1(class: /_heroHeading_/) }
-    element(:hero_text) { p(class: /_heroText_/) }
-    element(:hero_image) { img(class: /_heroImage_/) }
-    element(:hero_cta) { a(class: /_heroCta_/) }
+    element(:hero_section) { section(class: /_hero_/) }
+    element(:hero_title) { section(class: /_hero_/).h1(class: /_heroHeading_/) }
+    element(:hero_text) { section(class: /_hero_/).p(class: /_heroText_/) }
+    element(:hero_image) { section(class: /_hero_/).img(class: /_heroImage_/) }
+    element(:hero_cta) { section(class: /_hero_/).a(class: /_heroCta_/) }
     element(:content_wrapper) { section(class: /_wrapper_/) }
-    element(:latest_review_list) { ol(class: /_resultsList_/) }
-    element(:latest_review_card) { article(data_cy: 'product-card') }
-    element(:latest_review_card_name) { article(data_cy: 'product-card').h4.text }
-    element(:latest_review_card_link) { ol(class: /_resultsList_/).a(data_cy: 'product-info-link').href }
-    element(:latest_product_list) { div(class: /_products_/).ul }
-    element(:latest_product_card) { div(class: /_products_/).ul.li.a }
+    element(:benefits_section) { section(id: 'keyBenefits') }
+    element(:whats_new_section) { section(id: 'whatsNew') }
+    element(:latest_review_list) { section(id: 'whatsNew').ol(class: /_resultsList_/) }
+    element(:latest_review_card) { section(id: 'whatsNew').ol(class: /_resultsList_/).article(data_cy: 'product-card') }
+    element(:latest_review_card_name) { section(id: 'whatsNew').ol(class: /_resultsList_/).article(data_cy: 'product-card').h4.text }
+    element(:latest_review_card_link) { section(id: 'whatsNew').ol(class: /_resultsList_/).a(data_cy: 'product-info-link').href }
+    element(:latest_product_list) { section(id: 'whatsNew').div(class: /_products_/).ul }
+    element(:latest_product_card) { section(id: 'whatsNew').div(class: /_products_/).ul.li.a }
+    element(:newsletter_section) { section(id: 'newsletter') }
     # -- Flare Test Recorder --
     # https://www.testevolve.com/element-capture
 
