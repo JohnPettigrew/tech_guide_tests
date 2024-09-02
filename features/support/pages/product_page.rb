bf68b382d51d96cdf5167cfac09414759417c676
John Pettigrew <jpettigrew@parkinsons.org.uk>
2024-09-02 17:45:25 +0100

    Start to refactor product-page object

--- features/support/pages/product_page.rb
+++ features/support/pages/product_page.rb
@@ -4,12 +4,20 @@ module Pages
   end
 
   class ProductPage < TestEvolve::Core::PageObject
+    product_urls = {
+      parkinsons_on: '',
+      cue1plus: '' 
+    }
     element(:trusted_review_heading) { h2(text: "Trusted Review") }
     # -- Flare Test Recorder --
     # https://puk-tech-guide.vercel.app/catalogue
 
-    def visit
-      goto CATALOGUE_URL
+    def visit(product_name: nil)
+      if product_name.nil? 
+        goto CATALOGUE_URL
+      else 
+        goto product_urls[product_name.to_sym]
+      end
     end
 
     def wait_for_page_load(product_name:)
