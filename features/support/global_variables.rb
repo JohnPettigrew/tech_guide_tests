72cb3109e56bb2c5e00f594cdf3d13b7b91f4e23
John Pettigrew <jpettigrew@parkinsons.org.uk>
2024-09-11 11:06:27 +0100

    Tried to use TestEvolve.environment, without success

--- features/support/global_variables.rb
+++ features/support/global_variables.rb
@@ -1,4 +1,6 @@
 TECH_GUIDE_URL = 'https://techguide.parkinsons.org.uk/'
+# The variant below should work but doesn't - environment isn't being loaded when this script executes
+# TECH_GUIDE_URL = TestEvolve.environment['url']
 CATALOGUE_URL = "#{TECH_GUIDE_URL}catalogue"
 LEARN_URL = "#{TECH_GUIDE_URL}learn"
 HELP_URL = "#{TECH_GUIDE_URL}help"
