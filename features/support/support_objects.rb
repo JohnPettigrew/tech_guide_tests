bb835c4f09405f337dedbbb0b3c3c3f5b31e4331
John Pettigrew <jpettigrew@parkinsons.org.uk>
2024-09-16 13:37:35 +0100

    Slight refactor of RandomGenerator class

--- features/support/support_objects.rb
+++ features/support/support_objects.rb
@@ -3,7 +3,14 @@ class RandomGenerator
   attr_reader :email, :password
 
   def initialize
+    @email = @password = ''
+  end
+
+  def email
     @email = "#{SecureRandom.uuid}@mailinator.com"
+  end
+
+  def password
     @password = SecureRandom.base64(12)
   end
 end
